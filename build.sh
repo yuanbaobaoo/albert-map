79aaef76c5f2e44870a78462883a32bdc36d52a2
hodge <hodge.yuan@hotmail.com>
2025-02-21 17:17:44 +0800

    # 调整项目结构

--- build.sh
+++ build.sh
@@ -12,78 +12,68 @@ project="vmap-wiki-$1"
 projdir="site-$1"
 # 版本号
 version=$(date +"%Y%m%d%H%M")
+# 根目录
+workRoot=/data/xe-shell-cache/v-map-wiki
 # 工作目录
-workPath=/data/xe-shell-cache/v-map-wiki/$projdir
+workPath=$workRoot/$projdir
 
-mkdir -p $workPath
+# 删除缓存
+read -p "正在删除缓存目录 $workRoot 是否立即执行？(y/n) : " delOk
+if [ "$delOk" != "y" ]; then
+    exit 1
+fi
+
+rm -r $workRoot
+mkdir -p $workPath && cp -r . $workRoot/
 echo "npm编译参数: 工作目录=$workPath, 版本号=$version"
 
-# 遍历 workPath 下的所有一级目录文件，删除除了 node_modules 外的所有文件
-find "$workPath" -maxdepth 1 -print | while read item; do
-    if [ "$item" = "$workPath" ]; then
-        continue
-    fi
-
-    if [ "$item" = "$workPath/node_modules" ]; then
-        continue
-    fi
-
-    echo "正在删除缓存：$item"
-    rm -r $item
-done
-
-# 复制当前目录其他文件(只复制文件)
-find . -maxdepth 1 -type f ! -name "." ! -name ".gitignore" ! -name "build.sh"  -exec cp -r {} $workPath/ \;
+# 复制docker配置
+cp -r docker/ $workPath/
+# 删除其他文件
+rm -r $workRoot/.gitignore $workRoot/.git
 # 切换到项目目录
 cd $projdir
 
-# 复制当前目录文件到缓存目录
-find . -maxdepth 1 ! -name "node_modules" ! -name "." ! -name ".vitepress" ! -name "dist" -exec cp -r {} $workPath/ \;
-# 处理了一下.vitepress目录下的文件
-find .vitepress -maxdepth 1 ! -name "." ! -name "cache" -exec cp -r {} $workPath/.vitepress/ \;
-# 切换工作目录
-cd $workPath
-
-# 判断是否有插件，有的话就执行
-if [ -e plugin.sh ]; then
-  source plugin.sh
-fi
-
-# 安装依赖、编译
-npm i
-npm run build
-
-# 检查退出状态码
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-# 执行 docker镜像编译
-# nexus docker 仓库地址
-nexus_url=registry.cn-hongkong.aliyuncs.com/yuanbaobaoo/site
-# 是否删除本地镜像
-dellocal=true
-# 打包镜像
-echo "docker打包参数: project=$project, version=$version"
-
-# build
-echo "start: docker build -t $project ."
-docker build -q -t $project .
-
-# 检查退出状态码
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-# push
-imageName="$nexus_url:$project-$version"
-echo "docker tag $project $imageName"
-docker tag $project $imageName
-
-# 检查退出状态码
-if [ $? -ne 0 ]; then
-  exit 1
-fi
+## 判断是否有插件，有的话就执行
+#if [ -e plugin.sh ]; then
+#  source plugin.sh
+#fi
+#
+## 安装依赖、编译
+#npm i
+#npm run build
+#
+## 检查退出状态码
+#if [ $? -ne 0 ]; then
+#  exit 1
+#fi
+#
+## 执行 docker镜像编译
+## nexus docker 仓库地址
+#nexus_url=registry.cn-hongkong.aliyuncs.com/yuanbaobaoo/site
+## 是否删除本地镜像
+#dellocal=true
+## 打包镜像
+#echo "docker打包参数: project=$project, version=$version"
+#
+## build
+#echo "start: docker build -t $project ."
+#docker build -q -t $project .
+#
+## 检查退出状态码
+#if [ $? -ne 0 ]; then
+#  exit 1
+#fi
+#
+## push
+#imageName="$nexus_url:$project-$version"
+#echo "docker tag $project $imageName"
+#docker tag $project $imageName
+#
+## 检查退出状态码
+#if [ $? -ne 0 ]; then
+#  exit 1
+#fi
 
 # 如果本地镜像不保留，则上传镜像
 #if [ "$dellocal" != "false" ]; then
